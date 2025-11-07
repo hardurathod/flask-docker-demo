7c3a763c61c8b9f4aba8c86cba5945949288b24f
hardurathod <hardurathod11@gmail.com>
2025-11-07 20:23:52 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to hardik land"
+    return "Welcome to hardiks heart"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
